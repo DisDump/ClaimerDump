daaf4ab50d43b9a2749b35593df09f03b5f447a3
DisDump <157245551+DisDump@users.noreply.github.com>
2024-01-27 14:51:05 +1100

    Update index.js for multiple tokens

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 try {
     const { Client } = require('discord.js-selfbot-v13');
     const config = require('./config.json');
-    // change configuration in config.json file
+    // add categories here.
     const channelCategories = new Set(config.channelCategories);
-    const botID = config.botID;
+    const botIDs = config.botIDs;
 
-    const client = new Client({
-        checkUpdate: false,
-    });
+    for (let i = 0; i < config.tokens.length; i++) {
+        const client = new Client({
+            checkUpdate: false,
+        });
 
-    client.on('ready', async () => {
-        console.log(`ready with ${client.user.tag}`)
-    })
+        client.on('ready', async () => {
+            console.log(`ready with ${client.user.tag}`)
+        })
 
-    client.on('messageCreate', async (message) => {
-        if (channelCategories.has(message.channel.parentId) && message.author.id == botID) {
-            if (message.components && message.components[0]?.components) {
-                // console.log(message)
-                const buttons = message.components[0].components;
-                const claimButton = buttons.find(b => b.customId == 'claim');
-                console.log(claimButton);
-                // await claimButton.click(message);
-                await message.clickButton('claim');
+        client.on('messageCreate', async (message) => {
+            if (channelCategories.has(message.channel.parentId) && botIDs.includes(message.author.id)) {
+                if (message.components && message.components[0]?.components) {
+                    // console.log(message)
+                    const buttons = message.components[0].components;
+                    const claimButton = buttons.find(b => b.customId == 'claim');
+                    console.log(claimButton);
+                    // await claimButton.click(message);
+                    await message.clickButton('claim');
+                }
             }
-        }
-        if (message.author.id !== client.user.id) return;
-    });
-    client.on('channelCreate', async (channel) => {
-        if (channelCategories.has(channel.parentId) && channel.type == 'GUILD_TEXT') {
-            for (let i = 0; i < 3; i++) {
-                await channel.sendSlash(botID, 'claim');
+            if (message.author.id !== client.user.id) return;
+        });
+        client.on('channelCreate', async (channel) => {
+            if (channelCategories.has(channel.parentId) && channel.type == 'GUILD_TEXT') {
+                for (let i = 0; i < 3; i++) {
+                    await channel.sendSlash(botIDs, 'claim');
+                }
             }
-        }
-    });
+        });
 
-    client.login(config.token);
+        client.login(config.tokens[i]);
+    }
 } catch (e) {
     console.log(e);
 }
